95fbd3e5337a9f8764531fdf724fc690873042e8
stars1475 <3475075798@qq.com>
2023-12-28 13:01:13 +0800

    wangyi_UI

---
+++ src/music_search/Music.java
@@ -0,0 +1,33 @@
+package music_search;
+
+public class Music {
+    private String modifyTime;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String time;
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
---
+++ src/music_search/MusicLoader.java
@@ -0,0 +1,51 @@
+package music_search;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.Files.readAttributes;
+
+public class MusicLoader {
+    public static List<Music> loadAllMusicInfo() throws IOException {
+        String path = "resources/music";
+        File file = new File(path);
+        List<Music> list = new ArrayList<>();
+        if(file.isDirectory()){
+            String[] fileNames = file.list();
+            if (fileNames != null) {
+                for (String fileName : fileNames) {
+                    String musicPath = path + fileName;
+                    //System.out.println(musicPath);
+                    File musicFile = readMusicFile(musicPath);
+                    BasicFileAttributes attrs = readAttributes(musicFile.toPath(),BasicFileAttributes.class);
+                    String musicName = fileName.split("\\.")[0];
+                    String time = String.valueOf(attrs.size());
+                    String modifyTime = String.valueOf(attrs.lastModifiedTime());
+                    Music music = new Music();
+                    music.setTime(time);
+                    music.setTime(time);
+                    music.setModifyTime(modifyTime);
+                    music.setName(fileName);
+                    list.add(music);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static File readMusicFile(String path){
+        File file = new File(path);
+        return file;
+    }
+    public static void main(String[] args) throws IOException {
+        List<Music> list = MusicLoader.loadAllMusicInfo();
+        System.out.println(list);
+    }
+}
