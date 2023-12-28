237c85b72f1aa57609ff2f37afea2844f45f32a8
stars1475 <3475075798@qq.com>
2023-12-28 13:18:54 +0800

    wangyi_UI

---
+++ src/Main/Main_UI.java
@@ -0,0 +1,32 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main_UI extends Application {
+
+
+    public Main_UI() throws IOException {
+        URL url = new File("src/Main/Main_UI.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Stage stage = new Stage();
+        Scene scene = new Scene(root, 1277, 837);
+        //stage.initStyle(StageStyle.UTILITY);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/music_search/Music.java
@@ -0,0 +1,32 @@
+package music_search;
+
+public class Music {
+    private String modifyTime;
+    private String name;
+    private String time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
